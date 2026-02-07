e9a6d31b666cc59fa2825bceb1c5c4e96115d2eb
Deba-07 <dynamicdeba07@gmail.com>
2026-02-07 08:49:53 +0530

    feat: add BentoBox component with detailed features and images for Vapor75

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import BentoBox from "@/components/sections/bento-box/BentoBox";
 import Hero from "@/components/sections/hero/Hero";
 
 const page = () => {
@@ -9,6 +10,55 @@ const page = () => {
         body="Fall in love with the craft thanks to our professional grade keycaps and switches."
         ctaText="Buy vapor75"
       />
+
+      <BentoBox
+  heading="VAPOR75 FEATURES"
+  items={[
+    {
+      image: { src: "/images/render_6.png" },
+      title: "Full aluminum case.",
+      description:
+        "Premium materials for satisfying heft and durability.",
+      size: "Large",
+    },
+    {
+      image: { src: "/images/render_5.png" },
+      title: "Interchangeable knob system.",
+      description:
+        "Customize your control dial to click, scroll, or press.",
+      size: "Small",
+    },
+    {
+      image: { src: "/images/render_2.png" },
+      title: "Cross Platform.",
+      description:
+        "Mac, Windows, or Linux. Nimbus adapts to your workflow.",
+      size: "Medium",
+    },
+    {
+      image: { src: "/images/render_9.png" },
+      title: "Hot-Swappable Switches.",
+      description:
+        "Change your feel without any soldering.",
+      size: "Medium",
+    },
+    {
+      image: { src: "/images/render_11.png" },
+      title: "Custom Nimbus Keycap Profile.",
+      description:
+        "Designed for long coding sessions.",
+      size: "Small",
+    },
+    {
+      image: { src: "/images/render_3.png" },
+      title: "E-Ink Display Screen.",
+      description:
+        "Show battery, status, or a custom design.",
+      size: "Large",
+    },
+  ]}
+/>
+
     </>
   );
 };
